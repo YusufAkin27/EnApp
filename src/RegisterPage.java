e0939fee0b21b47592b2b7adad32383eb0777e53
yusufdumurcuk <yusufdumurcuk2@gmail.com>
2023-12-08 12:42:13 +0300

    aa

--- src/RegisterPage.java
+++ src/RegisterPage.java
@@ -8,7 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class RegisterPage {
-
+    private JFrame frame2;
     private JFrame frame;
     private JTextField nameAndSurnameField;
     private JTextField usernameField;
